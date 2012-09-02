e95f5de0221207bff07fa4e7f464aabcf6e32011
weishengshui <657620636@qq.com>
2012-09-02 15:24:42 +0800

    xxx

---
+++ src/main/java/com/chinarewards/posnet2/report/filter/ValidateCodeUsernamePasswordAuthenticationFilter.java
@@ -0,0 +1,16 @@
+package com.chinarewards.posnet2.report.filter;
+
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+public class ValidateCodeUsernamePasswordAuthenticationFilter extends
+		UsernamePasswordAuthenticationFilter {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
